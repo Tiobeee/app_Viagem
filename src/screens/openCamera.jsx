244ff06c5ca06fa9c40c1e07d5cef185373aeed8
Miguel <4xx136@gmail.com>
2024-04-08 09:27:17 -0300

    ss

--- src/screens/openCamera.jsx
+++ src/screens/openCamera.jsx
@@ -5,6 +5,8 @@ import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 export default function OpenCamera() {
   const [type, setType] = useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [capturedImage, setCapturedImage] = useState < any > (null);
 
   if (!permission) {
     return <View />;
@@ -23,12 +25,23 @@ export default function OpenCamera() {
     setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
   }
 
+  const __takePicture = async () => {
+    if (!camera) return
+    const photo = await camera.takePictureAsync()
+    console.log(photo)
+    setPreviewVisible(true)
+    setCapturedImage(photo)
+  }
+
   return (
     <View style={styles.container}>
       <Camera style={styles.camera} type={type}>
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
             <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.takeButton} onPress={__takePicture}>
+            <Text style={styles.text}>Tirar Foto</Text>
           </TouchableOpacity>
         </View>
       </Camera>
